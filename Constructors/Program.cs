ce6dd21b5f8c994591c5a398cab7c7558659e3f5
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-03-04 23:52:25 +0300

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+
+            //Customer customer3 = new Customer();
+            //customer3.Id = 1;
+            //customer3.FirstName = "Engin";
+            //customer3.LastName = "Demiroğ";
+            //customer3.City = "Ankara";
+
+            Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+
+        // default constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
